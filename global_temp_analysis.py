2f98102a204039f154ba665ec4218fc96d441768
Nico Tukiainen <nico.tukiainen@gmail.com>
2025-11-02 19:53:46 +0200

    convert time to Pandas datetime & Unix epoch

--- global_temp_analysis.py
+++ global_temp_analysis.py
@@ -4,8 +4,6 @@ import sqlite3
 url = "https://raw.githubusercontent.com/datasets/global-temp/master/data/monthly.csv"
 df = pd.read_csv(url)
 
-print(df.head())
-
 # Save to CSV
 df.to_csv("global_temp.csv", index=False)
 
@@ -13,3 +11,12 @@ df.to_csv("global_temp.csv", index=False)
 conn = sqlite3.connect("global_temp.db")
 df.to_sql("global_temperature", conn, if_exists="replace", index=False)
 conn.close()
+
+# Convert 'Year' column to datetime format
+df["Date"] = pd.to_datetime(df["Year"], format="%Y-%m")
+
+# Convert to Unix epoch
+df["UnixEpoch"] = df["Date"].astype("int64") // 10**9
+
+print(df.head())
+print(df.dtypes)
