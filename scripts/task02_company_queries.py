655e899f8a8465314ceac6adae3bdbb831f8ed45
Nico Tukiainen <nico.tukiainen@gmail.com>
2025-11-04 17:46:53 +0200

    query to calculate total salary costs per project

--- scripts/task02_company_queries.py
+++ scripts/task02_company_queries.py
@@ -4,6 +4,7 @@ import pandas as pd
 db_path = "data/company_data/company_database.db"
 conn = sqlite3.connect(db_path)
 
+# Query to join employee_projects and employees_realistic
 query_join = """
 SELECT 
     ep.project_id,
@@ -17,5 +18,19 @@ JOIN employees_realistic AS er
 
 df_join = pd.read_sql_query(query_join, conn)
 print(df_join.head())
+
+# Query to calculate total salary costs per project
+query_salary_costs = """
+SELECT
+    ep.project_id,
+    SUM((er.salary / 1900.0) * ep.hours_worked) AS salary_costs
+FROM employee_projects AS ep
+JOIN employees_realistic AS er
+    ON ep.employee_id = er.employee_id
+GROUP BY ep.project_id;
+"""
+
+df_costs = pd.read_sql_query(query_salary_costs, conn)
+print(df_costs.head())
 
 conn.close()
